fdef2b79d13951246a74674365af4e406f166a54
saurabh317 <saurabhsp317@gmail.com>
2024-03-30 18:10:33 +0530

    learnt ejs and templates

--- Projects/AdminPanel/index.js
+++ Projects/AdminPanel/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import express from 'express';
+import { ProductsController } from './src/controllers/products.controller.js';
+import path from "path";
+import ejsLayout from "express-ejs-layouts";// this is a middleware
+
+const controller = new ProductsController;
 
 const server = express();
 
-server.get('/', (req,res)=>{
-    return res.send('Welcome to Admin Panel');
-});
+// make the server aware about the view template engine
+server.set("view engine", 'ejs')
+server.set("views", path.join(path.resolve('src', 'views')))
 
-server.listen(3400);
+server.use(ejsLayout)
+
+server.use(express.static('src/views'));
+server.get('/', controller.getAllProducts);
+
+server.listen(3400, ()=> console.log("server has started"));
---
+++ Projects/AdminPanel/src/controllers/products.controller.js
@@ -0,0 +1,16 @@
+import path from 'path';
+import ProductsModel from '../models/products.models.js';
+
+export class ProductsController {
+  // this will return all the products available
+  getAllProducts(req, res){
+    const products = ProductsModel.get()
+    console.log(ProductsModel.get())
+    res.render("products", { products: products })
+
+    // down here as we are no longer rendering html so getting rid of it
+
+    // console.log(path.resolve('src', 'views', 'products.html'))
+    // res.sendFile(path.join(path.resolve('src', 'views', 'products.html')))
+  }
+}
